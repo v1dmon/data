ee95526484a1f906206cdd71848cae98de432667
resu <9valentino4@gmail.com>
2023-12-16 20:37:27 +0100

    flake squash

---
+++ analysis/README.md
@@ -0,0 +1,5 @@
+```bash
+$ nix develop
+$ cd ../benchmark
+$ poetry --directory ../analysis run jupyter notebook
+```
---
+++ analysis/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python3
+        poetry
+      ];
+      env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc
+        ];
+        POETRY_VIRTUALENVS_IN_PROJECT = "true";
+        POETRY_VIRTUALENVS_PATH = "./.venv";
+        POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = "true";
+      };
+    };
+  };
+}
